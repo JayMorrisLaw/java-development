b4b36802aaa5883ecb6f5863cc45f2869bd5c0ac
JayMorrisLaw <jaypoltergeist@gmail.com>
2025-04-10 09:27:53 -0400

    initial commit

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello everyone I am jaden and I like food");
+    }
+}
---
+++ workbook-1/shopping list exercise/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class ShoppingList{
+    public static void main(String[] args) {
+        System.out.println("eggs, juice, water, jam, bread, apples, veggies, soap, chicken, milk");
+        System.out.println("apples");
+        System.out.println("grapes");
+        System.out.println("chips");
+        System.out.println("berries");
+        System.out.println("cologne");
+        System.out.println("cups");
+    }
+}
