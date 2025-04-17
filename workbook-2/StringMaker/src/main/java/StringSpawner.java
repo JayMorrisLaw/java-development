1edde877c0beeeac408e552c5bac66bbb257340d
JayMorrisLaw <jaypoltergeist@gmail.com>
2025-04-17 13:20:24 -0400

    string builder practice

---
+++ workbook-2/StringMaker/src/main/java/StringSpawner.java
@@ -0,0 +1,21 @@
+public class StringSpawner {
+    public static void main(String[] args) {
+// creating and using string builder. use when you are making many changes to the string
+        StringBuilder skills = new StringBuilder();
+
+        skills.append("git ");
+        skills.append("HTML ");
+        skills.append("CCS ");
+        skills.append(" and Bootstrap ");
+        skills.append("javascript ");
+        skills.append("java ");
+        skills.append(" and express\n");
+
+// using a string builder instead of normal string saves memory
+
+        String mySkills = skills.toString();
+        System.out.println(mySkills);
+
+    }
+
+}
