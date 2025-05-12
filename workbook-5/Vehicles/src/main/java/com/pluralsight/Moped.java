366e1812e8e2f0265eea845e7bd000f28199426d
JayMorrisLaw <jaypoltergeist@gmail.com>
2025-05-12 13:49:32 -0400

    vehicles exercise

---
+++ workbook-5/Vehicles/src/main/java/com/pluralsight/Moped.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class Moped extends Vehicle {
+    private String color;
+    private int numberOfPassengers;
+    private int cargoCapacity;
+    private int fuelCapacity;
+    private boolean isChargedUp;
+    
+}
