83027fcfc669f66ae8d387cbed4461acd784f125
JayMorrisLaw <jaypoltergeist@gmail.com>
2025-06-09 14:53:50 -0400

    jdbc exercise

---
+++ workbook-8/NorthwindTraders/src/main/java/com/pluralsight/main.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+import java.sql.*;
+
+public class main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/northwind",
+                "root",
+                "P@sswOrd"
+        );
+
+        Statement statement = connection.createStatement();
+
+        String query = "SELECT ProductName FROM Products";
+
+        ResultSet results = statement.executeQuery(query);
+
+        while (results.next()) {
+            String products = results.getString("ProductName");
+            System.out.println(products);
+        }
+        connection.close();
+    }
+}
+
+
